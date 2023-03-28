cdbbfed7d49712400c8e67f3ff2851e34fde4a1b
Mostafa Elsayed <mme10@hw.ac.uk>
2023-03-28 13:32:59 +0100

    Implemented color changing

--- F21GP_CW2/Assets/Player.cs
+++ F21GP_CW2/Assets/Player.cs
@@ -19,19 +19,13 @@ public class Player : MonoBehaviour
     [SerializeField] float speed, shootForce;
     private float DirX, DirY;
     private string tempColour = "Default", baseColour = "Default", potionColour = "Default";
-    private Color finalColour;
+    //private Color finalColour;
     private bool holdingFlask;
     public int currentHealth, maxHealth;
 
     private PlayerAim playerAim;
 
-    // Costum Colours Used
-    public Color CostumBlue = new Color(0.1921569f, 0.3647059f, 0.9019608f);
-    public Color CostumRed = new Color(0.9372550f, 0.2470588f, 0.2509804f);
-    public Color CostumYellow = new Color(0.9607844f, 0.8705883f, 0.1960784f);
-    public Color CostumOrange = new Color(0.9058824f, 0.5725490f, 0.1568628f);
-    public Color CostumGreen = new Color(0.2156863f, 0.9490197f, 0.0000000f);
-    public Color CostumPurple = new Color(0.6823530f, 0.0784314f, 0.9686275f);
+    
 
     
 
@@ -67,8 +61,8 @@ public class Player : MonoBehaviour
 
 
         // Handle colour changes
-        ColourEval();
-        playerRend.color = finalColour;
+        //ColourEval();
+        //playerRend.color = finalColour;
 
         // Potion mechanic
 
@@ -85,19 +79,9 @@ public class Player : MonoBehaviour
     // Collisions
     void OnTriggerEnter2D(Collider2D col)
     {
-        // Debug info
-        Debug.Log("Player collied with " + col.gameObject.name + " @ " + Time.time);
-
-        // On entering a colour puddle set tempColour to the appropriate colour.
-        if (col.gameObject.name.Contains("BluePuddle"))
-            tempColour = "Blue";
-        else if (col.gameObject.name.Contains("RedPuddle"))
-            tempColour = "Red";
-        else if (col.gameObject.name.Contains("YellowPuddle"))
-            tempColour = "Yellow";
-
+    
         // Pickup empty flask
-        else if (col.gameObject.name.Contains("EmptyFlask"))
+        if (col.gameObject.name.Contains("EmptyFlask"))
         {
             holdingFlask = true;
             PotionUI.InstPotionUI(holdingFlask);
@@ -113,12 +97,7 @@ public class Player : MonoBehaviour
             SpawnEmptyFlask.SpawnPotion();
     }
 
-    void OnTriggerExit2D(Collider2D col)
-    {
-        // After leaving a colour puddle return tempColour to "default".
-        if (col.gameObject.name.Contains("Puddle"))
-            tempColour = "Default";
-    }
+    
 
     // Potion Mechanic
     private void PotionColours()
@@ -173,85 +152,5 @@ public class Player : MonoBehaviour
         }
     }
 
-    // Evaluate Colour changes and combinations
-    public void ColourEval()
-    {
-        switch (baseColour)
-        {
-            case "Default":
-                switch (tempColour)
-                {
-                    case "Blue":
-                        finalColour = CostumBlue;
-                        break;
-                    case "Red":
-                        finalColour = CostumRed;
-                        break;
-                    case "Yellow":
-                        finalColour = CostumYellow;
-                        break;
-                    default:
-                        finalColour = Color.black;
-                        break;
-                }
-                break;
-
-            case "Blue":
-                switch (tempColour)
-                {
-                    case "Blue":
-                        finalColour = CostumBlue;
-                        break;
-                    case "Red":
-                        finalColour = CostumPurple;
-                        break;
-                    case "Yellow":
-                        finalColour = CostumGreen;
-                        break;
-                    default:
-                        finalColour = CostumBlue;
-                        break;
-                }
-                break;
-
-            case "Yellow":
-                switch (tempColour)
-                {
-                    case "Blue":
-                        finalColour = CostumGreen;
-                        break;
-                    case "Red":
-                        finalColour = CostumOrange;
-                        break;
-                    case "Yellow":
-                        finalColour = CostumYellow;
-                        break;
-                    default:
-                        finalColour = CostumYellow;
-                        break;
-                }
-                break;
-            case "Red":
-                switch (tempColour)
-                {
-                    case "Blue":
-                        finalColour = CostumPurple;
-                        break;
-                    case "Red":
-                        finalColour = CostumRed;
-                        break;
-                    case "Yellow":
-                        finalColour = CostumOrange;
-                        break;
-                    default:
-                        finalColour = CostumRed;
-                        break;
-                }
-                break;
-
-            default:
-                // Nothing for now...
-                break;
-        }
-    }
+    
 }
