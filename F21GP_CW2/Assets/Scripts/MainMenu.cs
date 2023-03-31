a0453eee1d32450efe69e33bdc0b96e5f959610c
Georgios Pavlis <geomp03@gmail.com>
2023-03-31 14:21:16 +0100

    Main menu
    Added basic main menu scene.

---
+++ F21GP_CW2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private AudioClip uiButton;
+    private SoundEffectSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = FindObjectOfType<SoundEffectSource>();
+    }
+
+    public void GameButton()
+    {
+        audioSource.PlaySoundEffect(uiButton);
+        SceneManager.LoadScene("Room_1", LoadSceneMode.Single);
+        // SceneManager.LoadScene("Dungeon", LoadSceneMode.Single);
+    }
+
+    public void SettingsButton()
+    {
+        // Settings menu maybe...
+        Debug.Log("Settings menu");
+    }
+
+    public void QuitButton()
+    {
+        audioSource.PlaySoundEffect(uiButton);
+        Application.Quit();
+    }
+}
