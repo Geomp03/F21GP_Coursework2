298955389ffa73913630d2485f90651c4ba50eee
Georgios Pavlis <geomp03@gmail.com>
2023-04-03 11:24:24 +0100

    Bug fixing
    Fixed bug where loot would spawn in rooms before it should.

--- F21GP_CW2/Assets/Scripts/RoomController.cs
+++ F21GP_CW2/Assets/Scripts/RoomController.cs
@@ -8,7 +8,7 @@ public class RoomController : MonoBehaviour
     [SerializeField] private List<GameObject> roomLoot = new List<GameObject>();
 
     private int enemyCount;
-    private bool lootSpawned = false;
+    private bool lootSpawned = false, playerEntered = false, enemySpawned = false;
     private Vector3 temp;
     private Vector3 spawnLocation;
 
@@ -23,15 +23,21 @@ public class RoomController : MonoBehaviour
 
     private void Update()
     {
-        if (enemyCount == 0 && lootSpawned == false)
+        if (enemyCount > 0)
+            enemySpawned = true;
+
+        // Three conditions, all enemies in the room killed, player has entered it for the first time and loot should spawwn once.
+        if (enemyCount == 0 && lootSpawned == false && playerEntered == true && enemySpawned == true)
         {
             Debug.Log("Room cleared!!");
+
             for (int i = 0; i < roomLoot.Count; i++)
             {
                 temp = Random.insideUnitCircle * 5;
-                spawnLocation = transform.position + temp;   // Select a random spot to spot the loot
+                spawnLocation = transform.position + temp;   // Select a random spot to spawn the loot
                 Instantiate(roomLoot[i], spawnLocation, Quaternion.identity);  // Spawn loot instance
             }
+
             audioSource.PlaySoundEffect(lootSFX);
             lootSpawned = true;
         }
@@ -44,6 +50,9 @@ public class RoomController : MonoBehaviour
             enemyCount++;
             Debug.Log(enemyCount);
         }
+
+        if (collision.tag == "Player")
+            playerEntered = true;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
